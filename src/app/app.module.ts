9e276793a259222b31c318274ac5c4046fe0cbd1
unknown <samuel@health263.systems>
2020-07-29 23:11:34 +0200

    app module file added

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import{ 
+  MatInputModule, 
+  MatCardModule, 
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatPaginatorModule,
+  //MatProgressSpinner
+} from '@angular/material';
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { from } from 'rxjs';
+import { AppRoutingModule } from './app.routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    PostCreateComponent,
+    PostListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatPaginatorModule,
+    //MatProgressSpinner,
+    HttpClientModule
+  ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
